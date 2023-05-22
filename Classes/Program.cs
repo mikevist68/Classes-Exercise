b6a332b394ce859d91efc64cc828db38a0d87fb5
Michael Vistocci <mikevist68@yahoo.com>
2023-05-22 15:43:32 -0400

    completed

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+
+            Car car1 = new Car();
+
+            car1.Make = "toyota";
+            car1.Model = "camry";
+            car1.Year = 2014;
+
+            Console.WriteLine($"Hello, my car is a {car1.Make} The model is {car1.Model} and it is a {car1.Year}.");
+
+
         }
     }
 }
